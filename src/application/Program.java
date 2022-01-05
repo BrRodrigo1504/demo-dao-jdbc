8592778df5e3aa4145d99e95606dd89b4e8ae110
Rodrigo Fernandes <rodrigops1504@gmail.com>
2022-01-05 18:36:47 +0000

    FindAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== TEST 3: seller FindAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
